8d9406f289e4b2a6be1c753b242264cdf40b786c
Madhur batra <madhurbatra097@gmail.com>
2021-01-19 15:20:30 +0530

    Minor updates

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/auth0/go-jwt-middleware v1.0.0
+	github.com/chromedp/chromedp v0.6.4
 	github.com/form3tech-oss/jwt-go v3.2.2+incompatible
 	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19
 	github.com/gin-gonic/gin v1.6.3
---
+++ scraper/scraper.go
@@ -0,0 +1,52 @@
+package scraper
+
+import (
+	"context"
+	"strings"
+	"time"
+
+	"github.com/chromedp/chromedp"
+	log "github.com/sirupsen/logrus"
+)
+
+func FetchPrice(q string, res chan<- string) {
+
+	log.WithFields(log.Fields{
+		"Stock": q,
+	}).Debug("Fetch stock price")
+
+	url := "https://www.google.com"
+	inQ := q + " stock price"
+	inTextSel := `//input[@name='q']`
+	btnSel := `input[name="btnK"]`
+	outTextSel := `//span[@jsname="vWLAgc"]`
+
+	// create context
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+	// Wait for timeout.
+	timeoutContext, _ := context.WithTimeout(ctx, 20*time.Second)
+
+	// run task list
+	var result string
+	err := chromedp.Run(
+		timeoutContext,
+		chromedp.Navigate(url),
+		chromedp.WaitVisible(inTextSel),
+		chromedp.SendKeys(inTextSel, inQ),
+		chromedp.Click(btnSel, chromedp.ByQuery),
+		chromedp.WaitVisible(outTextSel),
+		chromedp.Text(outTextSel, &result),
+	)
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"Stock": strings.Title(q),
+			"Error": err,
+		}).Error("Error scrapping stock price")
+		res <- ""
+		return
+	}
+
+	res <- result
+}
